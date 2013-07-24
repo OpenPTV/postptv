3625e504b745ccf8134484afa3588d560cdb0758
Yosef Meller <mellerf@netvision.net.il>
2013-07-24 09:43:13 +0300

    Janitoring

--- flowtracks/interpolation.py
+++ flowtracks/interpolation.py
@@ -42,7 +42,6 @@ def select_neighbs(tracer_pos, interp_points, radius=None, num_neighbs=None):
         dist_sort = np.argsort(dists, axis=1)
         use_parts = np.zeros(dists.shape, dtype=np.bool)
         
-        # num_neighbs + 1 because the list still includes self-distance (0).
         use_parts[np.repeat(np.arange(interp_points.shape[0]), num_neighbs),
             dist_sort[:,:num_neighbs].flatten()] = True
     
