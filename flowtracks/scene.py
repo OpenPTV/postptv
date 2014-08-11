c40bda102109ff87e5e4959d138ec590dddcc105
Yosef Meller <mellerf@netvision.net.il>
2014-08-11 14:57:33 +0300

    Add a dualScene class comparable to the old sequence class, working on HDF files only.

--- flowtracks/scene.py
+++ flowtracks/scene.py
@@ -14,7 +14,10 @@ Created on Sun Aug 10 11:28:42 2014
 """
 
 import itertools, tables, numpy as np
+from ConfigParser import SafeConfigParser
+
 from .trajectory import Trajectory, ParticleSnapshot
+from .particle import Particle
 
 class Frame(object):
     pass
@@ -94,31 +97,92 @@ class Scene(object):
             yield ParticleSnapshot(**kwds)
         
 
-def dual_scene_iterator(particle_scene, tracer_scene, frame_range=-1):
+class DualScene(object):
     """
-    Iterates over a scene represented by two HDF files (one for inertial 
-    particles, one for tracers), and returns a Frame object whose two
-    attributes (.tracers, .particles) hold a corresponding ParticleSnapshot
-    object.
+    Holds a scene orresponding to the dual-PTV systems, which shoot separate
+    but coordinated streams for the tracers data and inertial particles data.
+    """
+    def __init__(self, tracers_path, particles_path, frate, particle,
+        frame_range=None):
+        """
+        Arguments:
+        tracers_path, particles_path - respectively the path to the tracers 
+            and particles HDF files.
+        frate - frame rate at which the scene was shot, [1/s].
+        particle - a Particle object describing the inertial particles'
+            diameter and density.
+        frame_range - a uniform frame range to set to both of them. The
+            default is None, meaning to use all frames (assuming 
+            equal-length data streams)
+        """
+        self.frate = frate
+        self.part = particle
+        
+        self._paths = (tracers_path, particles_path)
+        self._tracers = Scene(tracers_path, frame_range)
+        self._particles = Scene(particles_path, frame_range)
+        self._rng = frame_range # for restoring it after iteration.
+    
+    def get_particles_path(self):
+        return self._paths[1]
+    
+    def iter_frames(self, frame_range=-1):
+        """
+        Iterates over a scene represented by two HDF files (one for inertial 
+        particles, one for tracers), and returns a Frame object whose two
+        attributes (.tracers, .particles) hold a corresponding
+        ParticleSnapshot object.
+        
+        Arguments:
+        particle_scene, tracer_scene - each a Scene object for the 
+            corresponding file.
+        frame_range - tuple (first, last) sets the frame range of both scenes
+            to an identical frame range. Argument format as in 
+            Scene.set_frame_range(). Default is (-1) meaning to skip this. 
+            Then the object's initialization range is used, so initialize
+            to a coordinated range if you use the default.
+        
+        Yields:
+        the Frame object for each frame in turn.
+        """
+        if frame_range != -1:
+            self._particles.set_frame_range(frame_range)
+            self._tracers.set_frame_range(frame_range)
+    
+        for particles, tracers in itertools.izip(
+            self._particles.iter_frames(), self._tracers.iter_frames()):
+            frame = Frame()
+            frame.tracers = tracers
+            frame.particles = particles
+            yield frame
+        
+        # restore original frame range.
+        if frame_range != -1:
+            self._particles.set_frame_range(self._frame_range)
+            self._tracers.set_frame_range(self._frame_range)
+
+def read_dual_scene(conf_fname):
+    """
+    Read dual-scene parameters, such as unchanging particle properties and
+    frame range. Values are stored in an INI-format file.
     
     Arguments:
-    particle_scene, tracer_scene - each a Scene object for the corresponding
-        file.
-    frame_range - tuple (first, last) sets the frame range of both scenes to
-        an identical frame range. Argument format as in 
-        Scene.set_frame_range(). Default is (-1) meaning to skip this. Only
-        use that if you know your scenes to be coordinated.
+    conf_fname - name of the config file
     
-    Yields:
-    the Frame object for each frame in turn.
+    Returns:
+    a DualScene object initialized with the configuration values found.
     """
-    if frame_range is not None:
-        particle_scene.set_frame_range(frame_range)
-        tracer_scene.set_frame_range(frame_range)
+    parser = SafeConfigParser()
+    parser.read(conf_fname)
 
-    for tracers, particles in itertools.izip(
-        particle_scene.iter_frames(), tracer_scene.iter_frames()):
-        frame = Frame()
-        frame.tracers = tracers
-        frame.particles = particles
-        yield frame
+    particle = Particle(
+        parser.getfloat("Particle", "diameter"),
+        parser.getfloat("Particle", "density"))
+    
+    frate = parser.getfloat("Scene", "frame rate")
+    tracer_file = parser.get("Scene", "tracers file")
+    part_file = parser.get("Scene", "particles file")
+    frange = (parser.getint("Scene", "first frame"),
+        parser.getint("Scene", "last frame") + 1)
+    
+    return DualScene(tracer_file, part_file, frate, particle, frange)
