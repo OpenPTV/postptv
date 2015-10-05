0b57b0a111e8e59c0e88ad5d77831842971b41be
Yosef Meller <mellerf@netvision.net.il>
2015-10-05 12:31:21 +0300

    List of mock modules much expanded.

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,8 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'tables']
+MOCK_MODULES = ['numpy', 'numpy.ma', 'scipy', 'scipy.interpolate', 'scipy.io',
+    'tables', 'matplotlib', 'matplotlib.pyplot']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
