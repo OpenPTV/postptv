8c5a07cdd18c740afc206eb1e4b754d89ed46e6d
Yosef Meller <yosefmel@post.tau.ac.il>
2017-02-23 10:35:47 +0200

    Fix sphynx not recognizing use of scipy.spatial for the KDTree branch.

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
             return Mock()
 
 MOCK_MODULES = ['numpy', 'numpy.ma', 'scipy', 'scipy.interpolate', 'scipy.io',
-    'tables', 'matplotlib', 'matplotlib.pyplot']
+    'scipy.spatial', 'tables', 'matplotlib', 'matplotlib.pyplot']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
